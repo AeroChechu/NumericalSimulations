2a07546c1b63e8342f3162d80083793b502cc0c4
AeroChechu <cecilia.tomasetti@hotmail.it>
2015-10-18 18:37:55 +0200

    WIP

--- NumericalSimulations/PengRobinson.h
+++ NumericalSimulations/PengRobinson.h
@@ -1,17 +1,4 @@
 #pragma once
-#include <vector>
 
-void PengRobinson(double p0, double rho_min, double rho_max, int Npts, double omega, double t_cr, double p_cr)
-{
-	double delta_rho = (rho_max - rho_min) / Npts;
-	vector<double> rho(Npts);
-	double Rgas=8.314
 
-	double Temp{};
-	for (int i = 0; i < Npts; i++)
-	{
-		rho[i] = rho_min + i*delta_rho;
-		appo=
-	}
-
-}
+void PengRobinson(double p0, double rho_min, double rho_max, int Npts, double omega, double t_cr, double p_cr, double tol);
